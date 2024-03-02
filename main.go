7e0a1f4ecc2d147850d85e7e4d598710e4eaaa5b
fiknaufalh <18221096@mahasiswa.itb.ac.id>
2024-03-03 01:17:08 +0700

    feat: add router for users and photos

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
-import (
-	// "fmt"
-	"profile-picture-api/database"
-	"profile-picture-api/router"
-)
+import "profile-picture-api/router"
 
-func init() {
-	database.ConnectDatabase()
-}
 
 func main() {
 	r := router.SetupRouter()
-	r.Run(":5000")
+	r.Run(":5432")
 }
--- router/router.go
+++ router/router.go
@@ -1,15 +1,41 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"profile-picture-api/controllers"
+	"profile-picture-api/database"
+	"profile-picture-api/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello, World!",
+			"message": "Welcome to Profile Picture API!",
+			"author":  "Fikri Naufal Hamdi",
 		})
 	})
-	
+
+	database.ConnectDatabase()
+
+	router.POST("/users/register", controllers.Register)
+	router.POST("/users/login", controllers.Login)
+	router.POST("/users/logout", controllers.Logout)
+
+	authorized := router.Group("/")
+	authorized.Use(middlewares.CheckAuth())
+	{
+		authorized.GET("/users/:id", controllers.GetUserbyID)
+		authorized.POST("/users/:id", controllers.UpdateUser)
+		authorized.DELETE("/users/:id", controllers.DeleteUser)
+
+		authorized.GET("/photos", controllers.GetAllPhotos)
+		authorized.POST("/photos", controllers.CreatePhoto)
+		authorized.PUT("/photos/:id", controllers.UpdatePhoto)
+		authorized.DELETE("/photos/:id", controllers.DeletePhoto)
+	}
+
 	return router
 }
